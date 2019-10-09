4c31d24ec44e0a22a8b4ed21573dcae295d813bc
Unknown <lacknersmichael@gmail.com>
2019-10-09 10:17:16 +0200

    added generic model

--- src/reservoirs.jl
+++ src/reservoirs.jl
@@ -16,3 +16,59 @@ function model_A(du,u,p,t)
     du[4] = p[2,4] * u[2] + p[3,4] * u[3] - p[4,2] * u[4] - p[4,3] * u[4] - p[4,5] * u[4]
     du[5] = p[2,5] * u[2] + p[3,5] * u[3] + p[4,5] * u[4] - p[5,1] * u[5]
 end
+
+
+function model(du,u,p,t)
+    # indices of du, u, p:
+    # 1:    heat bath
+    # 2:    ground state
+    # 3:    pumped mode
+    # 4+:   additional modes
+
+    function pumpfun(t)
+        if -20 < t <= 0
+            f = (t+20) / 20
+        elseif 0 < t < 10
+            f = -(t-10) / 10
+        else
+            f = 0.0
+        end
+        return f
+    end
+
+    # Number of Reservoirs
+    N = length(du)
+
+    # HEAT BATH
+    # <-- pumped mode [3], additional modes[4+]
+    # --> ground state [2]
+    du[1] = -u[1] * p[1,2]
+    for i = 3:N
+        du[1] += u[i] * p[i,1]
+    end
+
+    # GROUND STATE
+    # <-- heat bath [1]
+    # --> pumped mode [3]
+    du[2] = u[1] * p[1,2] - (u[2] - u[3]) * p[2,3] * pumpfun(t)
+
+    # PUMPED MODE
+    # <-- ground state [2], additional modes[4+]
+    # --> heat bath [1], additional modes[4+] (in next block)
+    # from ground state / to heat bath:
+    du[3] = (u[2] - u[3]) * p[2,3] * pumpfun(t)
+
+    # PUMPED MODE +
+    # ADDITIONAL MODES
+    for i = 4:N
+        du[i] = 0.0
+    end
+    for n = 3:N
+        du[n] -= u[n] * p[n,1] # to heat bath
+        for i = 3:N
+            n == i && continue
+            du[n] += u[i] * p[i,n] # from other mode
+            du[n] -= u[n] * p[n,i] # to other mode
+        end
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -105,3 +105,84 @@ end
     sol = solve(prob);
     @test isapprox(sol[end][1], 1.0, atol=1e-4)
 end
+
+
+@testset "General Model" begin
+      tstart = -30.0
+      tend = 270.0
+      tspan = (tstart, tend)
+
+      # All in ground state and pumping hard. We shoud end up with
+      # 1/2 in both ground and excited state.
+      u0 = [0, 1.0, 0]
+      p = spzeros(3,3)
+      p[2,3] = 1.0
+      prob = ODEProblem(model, u0, tspan, p)
+      sol = solve(prob)
+      @test isapprox(sol[end][2], 0.5, atol=1e-4) &&
+            isapprox(sol[end][3], 0.5, atol=1e-4)
+      @test sum(sol[end]) ≈ 1.0
+
+      # All in excited state and pumping hard. We shoud end up with
+      # 1/2 in both ground and excited state.
+      u0 = [0, 0.0, 1.0]
+      p = spzeros(3,3)
+      p[2,3] = 1.0
+      prob = ODEProblem(model, u0, tspan, p)
+      sol = solve(prob)
+      @test isapprox(sol[end][2], 0.5, atol=1e-4) &&
+            isapprox(sol[end][3], 0.5, atol=1e-4)
+      @test sum(sol[end]) ≈ 1.0
+
+      # All in ground state. Pump and let equilibrate in all additional
+      # modes. Including the ground state we should have 1/10 everywhere.
+      N = 11 # total number of states
+      u0 = zeros(N)
+      u0[2] = 1.0
+      p = spzeros(N,N)
+      p[2,3] = 1.0
+      for i = 3:N, j=3:N
+            i == j && continue
+            p[i,j] = 1.0
+      end
+      prob = ODEProblem(model, u0, tspan, p, maxiter=1)
+      sol = solve(prob)
+      for i = 2:11
+            @test isapprox(sol[end][i], 1/10, atol=1e-4)
+      end
+      @test sum(sol[end]) ≈ 1.0
+
+      # All in excited state and pumping hard. We shoud end up with
+      # 1/2 in both ground and excited state.
+      u0 = [0, 0.0, 1.0]
+      p = spzeros(3,3)
+      p[2,3] = 1.0
+      prob = ODEProblem(model, u0, tspan, p)
+      sol = solve(prob)
+      @test isapprox(sol[end][2], 0.5, atol=1e-4) &&
+            isapprox(sol[end][3], 0.5, atol=1e-4)
+      @test sum(sol[end]) ≈ 1.0
+
+      # All in ground state. Pump and let equilibrate in all additional
+      # modes + heat bath. Heat bath routes to ground state again. We should
+      # end up with all in ground state.
+      N = 6 # total number of states
+      u0 = zeros(N)
+      u0[2] = 1.0
+      p = spzeros(N,N)
+      p[2,3] = 1.0
+      for i = 3:N
+            p[i,1] = 1.0 # all to heat bath
+      end
+      p[1,2] = 1.0 # heat bath to ground state
+      for i = 3:N, j=3:N
+            i == j && continue
+            p[i,j] = 1.0
+      end
+      global p
+      prob = ODEProblem(model, u0, tspan, p, maxiter=1)
+      sol = solve(prob)
+      @show sol[end]
+      @test sol[end][2] ≈ 1.0
+      @test sum(sol[end]) ≈ 1.0
+end
